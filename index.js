f9ad14b5e6bf8afcf2e62a3e244dde9a34126011
Damon Brown <dbrow437@gmail.com>
2018-02-08 02:38:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  var newKittens = [kittens.slice("Garfield")]
+  var newKittens = [kittens.slice(0, kittens"Garfield")]
   return newKittens
 }
