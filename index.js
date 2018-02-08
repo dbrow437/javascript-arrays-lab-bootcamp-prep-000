f24aaa2d9bc2c214f6b7f703b77a24f8c8c9b661
Damon Brown <dbrow437@gmail.com>
2018-02-08 03:11:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ function removeLastKitten(){
 }
 
 function removeFirstKitten(){
-  var newKittens = kittens.slice(0, kittens.length)
+  var newKittens = kittens.slice(1, kittens.length)
   return newKittens
 }
