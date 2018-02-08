526b896e81a80b9bb17480a659283faa3752b736
Damon Brown <dbrow437@gmail.com>
2018-02-08 02:17:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function destructivelyRemoveFirstKitten(){
 var kittens = ["Milo", "Otis", "Garfield"] 
 
 function appendKitten(name){
-  var NewKittens
+  var NewKittens = ["Milo", "Otis", "Garfield", "Broom"]
   return("appenKitten")
 }
